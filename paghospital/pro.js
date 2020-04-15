f4811ea769af7b2af1bad062d1868be42267990d
brandonortiz15 <61805906+brandonortiz15@users.noreply.github.com>
2020-04-15 15:45:23 -0500

    Add files via upload

--- paghospital/pro.js
+++ paghospital/pro.js
@@ -1,3 +1,31 @@
-function enviar() {
+function sesion() {
+	document.getElementById("vent").style.display="block";
 	
+}
+
+function cerrar() {
+	document.getElementById("vent").style.display="none";
+}
+
+function iniciar(){
+	var nombr = document.getElementById("nombre").value;
+	var contr = document.getElementById("contra").value;
+	var usua =  document.getElementById("usuario").value;
+	
+	
+
+	if (contr==="" && nombr=="") {
+		window.alert("Rellena todos los campos");
+	}
+
+	
+
+	if (nombr=="Alejandro" && contr=="123" ) {
+	document.getElementById("vent").style.display="none";
+	document.getElementById("sesion").innerHTML = " Alejandro Lopez";
+
+
+	}else{
+		window.alert("Usuario o contraseña no son validos");
+	}
 }
