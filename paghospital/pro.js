a329df43e67e3860e54121b9ec4c0d017b92139b
brandonortiz15 <61805906+brandonortiz15@users.noreply.github.com>
2020-04-21 17:07:20 -0500

    Add files via upload

--- paghospital/pro.js
+++ paghospital/pro.js
@@ -10,36 +10,73 @@ function cerrar() {
 function iniciar(){
 	var nombr = document.getElementById("nombre").value;
 	var contr = document.getElementById("contra").value;
-	var usua =  document.getElementById("usuario").value;
-	
+		
 	
 
-	if (contr==="" && nombr=="") {
+	if (contr==="" && nombr==""){
 		window.alert("Rellena todos los campos");
 	}
 
 	
 
-	if (nombr=="Alejandro" && contr=="123" ) {
+	if (nombr=="Alejandro" && contr=="1234" ) {
 	document.getElementById("vent").style.display="none";
-	document.getElementById("sesion").innerHTML = " Alejandro Lopez";
+	document.getElementById("sesi").innerHTML ="Alejandro";
 
 
-	}else{
+	}
+	else{
 		window.alert("Usuario o contraseña no son validos");
 	}
 }
 
 function enviar(){
 
-	 event.preventDefault();
-	
-	var n = document.getElementById("nom").value;
-	var a = document.getElementById("asunto").value;
-	var e = document.getElementById("email").value;
-	var t = document.getElementById("telefono").value;
-	var h = document.getElementById("hospital").value;
-	var m = document.getElementById("mensaje").value;
 
-	console.log("n");
-}
+			var info=[], g=0;	
+	 info[0]= document.getElementById("nom").value;
+	 info[1]= document.getElementById("asunto").value;
+	 info[2]= document.getElementById("email").value;
+	 info[3]= document.getElementById("telefono").value;
+	 info[4]= document.getElementById("hospital").value;
+	 info[5]= document.getElementById("mensaje").value;
+
+			
+			
+			for(var i=0;i < 6;i++){
+			
+				if(info[i]==""){g++}
+			
+			}
+			
+			for(var i=0;i < 4;i++){
+			
+			console.log(info[i]);
+			
+			}
+		}
+
+		function enviar(){
+
+
+			var info=[], g=0;	
+	 info[0]= document.getElementById("name").value;
+	 info[1]= document.getElementById("gmail").value;
+	 info[2]= document.getElementById("ciudad").value;
+	 info[3]= document.getElementById("edad").value;
+	 info[4]= document.getElementById("sexo").value;
+	 
+			
+			
+			for(var i=0;i < 5;i++){
+			
+				if(info[i]==""){g++}
+			
+			}
+			
+			for(var i=0;i < 4;i++){
+			
+			console.log(info[i]);
+			
+			}
+		}
